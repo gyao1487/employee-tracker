03c5f8cfeff0cb4b8557cc765b717673c524cf4c
Grace <112779767+grachimus@users.noreply.github.com>
2022-12-18 15:01:28 -0600

    created schema.sql

--- db/schema.sql
+++ db/schema.sql
@@ -4,9 +4,30 @@ CREATE DATABASE employees_db;
 
 USE employees_db;
 --Create department table
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) NOT NULL
+);
 
 -- Create role table
-
+CREATE TABLE role (
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR (30),
+    salary DECIMAL,
+    department_id INT,
+    FOREIGN KEY (department_id)
+    REFERENCES department(id)
+    ON DELETE SET NULL
+);
 -- Create employee table
-
+CREATE TABLE employee (
+    id INT, 
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE SET NULL,
+    manager_id INT
+)
 
